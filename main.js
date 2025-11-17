7ca87820240d64cba6b8e5017b51fdedd016656d
Niels Middel <e228440@sbl.ch>
2025-11-17 09:20:06 +0000

    hello world

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log("Hello World")
+
+let counter = 0;
+function createNewElement("p")
+
+counter = counter + 1;
+
+createNewElement.textContent = "Element " + counter
+
+let mainElement = document.querySelector("main");
+mainElement.appendChild(newElement);
+}
